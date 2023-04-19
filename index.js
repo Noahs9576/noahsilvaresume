97d906cd3b3b0765dff4a386bf6d96cdb56cd160
Noah Silva <noahsilva@noahs-mbp.home>
2023-04-19 17:26:44 -0400

    vue

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const path = require('path')
+
+const fs = require('fs');
+const vuebuildfiles = fs.readdirSync(path.resolve('./dist/assets'));
+
 const port = 3000;
+
+
 
 //localhost:3000
 app.get('/', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.sendFile(__dirname + '/resources/index.html');
+    res.sendFile(path.resolve('./dist/index.html'));
     
 });
+
+app.get('/assets/:filename', function(req, res){
+    console.log(req.params.filename)
+    res.sendFile(path.resolve('./dist/assets/' + req.params.filename));
+});
+
+app.get('/favicon.ico', (req,res) => {
+    res.sendFile(path.resolve('./dist/favicon.ico'));
+})
 
 app.listen(port , () => {
     console.log(`Backend is running on port ${port}`);
---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+
+export default {
+  data() {
+    return {
+      count: 1
+    }
+  },
+
+  mounted() {
+
+  }
+}
+</script>
+
+<template>
+  <button @click="count++">Count is {{ count }}</button>
+</template>
+
+<style scoped>
+
+</style>
